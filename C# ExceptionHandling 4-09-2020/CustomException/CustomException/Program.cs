8a5bc071113d1e40f4c34e1086a9b32058963983
Bhaskar5959 <bhaskar221998@gmail.com>
2020-09-05 10:35:26 +0530

    add exceptions

---
+++ C# ExceptionHandling 4-09-2020/CustomException/CustomException/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CustomException
+{
+    class InvalidAgeException : Exception
+    {
+        public InvalidAgeException() : base() { }
+        public InvalidAgeException(string msg) : base(msg) { }
+
+    }
+    class Person
+    {
+        private int age;
+        public int Age
+        {
+            set
+            {
+                if (value >= 19)
+                    age = value;
+                else
+                {
+                    InvalidAgeException Obj = new InvalidAgeException("you are not eligible to vote");
+                    throw Obj;
+                }
+            }
+            get
+            {
+                return age;
+            }
+        }
+    }
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.Write("Enter the age : ");
+            try
+            {
+                string ageInput = Console.ReadLine();
+
+                person.Age = int.Parse(ageInput);
+                Console.WriteLine("you are eligible for vote");
+            }
+            catch (InvalidAgeException expObj)
+            {
+                Console.WriteLine(expObj.Message);
+            }
+
+        }
+    }
+}
---
+++ C# ExceptionHandling 4-09-2020/exceptionhandling/exceptionhandling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exceptionhandling
+{
+    class TryCatch
+    {
+        static void ProcessString(string s)
+        {
+            if (s == null)
+            {
+               throw new ArgumentNullException();
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string s = null; 
+
+            try
+            {
+                ProcessString(s);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+            }
+        }
+    }
+
+   
+}
