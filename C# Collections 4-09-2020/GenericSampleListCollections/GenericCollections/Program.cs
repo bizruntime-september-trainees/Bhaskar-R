51d84652d27af995acf9f2a70edbe4e43015fb30
Bhaskar5959 <bhaskar221998@gmail.com>
2020-09-06 16:20:47 +0530

    commit basic collections

---
+++ C# Collections 4-09-2020/GenericSampleListCollections/GenericCollections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            List<String> list = new List<string>();
+            list.Add("Bhaskar");
+            list.Add("Aman");
+            list.Add("Rakhesh");
+            list.Add("Utkarsh");
+            foreach(String name in list)
+            {
+                Console.WriteLine("list elements are:" + name);
+            }
+            
+        }
+    }
+    class Program2
+    {
+        public static void main(String[] args)
+        {
+            List<String> list1 = new List<string> { "Bhaskar", "Sandeep", "Suneel", "vishnu" };
+
+            foreach (String names in list1)
+            {
+                Console.WriteLine(names);
+
+            }
+        }
+    }
---
+++ C# Collections 4-09-2020/ListMethods/listexamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listexamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> list = new List <int>();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            for(int i=0;i<=list.Count-1;i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine("After remove element in list:");
+            list.Remove(20);
+            foreach(int list1 in list)
+            {
+                Console.WriteLine(list1);
+            }
+            Console.WriteLine("Finished");
+        }
+    }
+}
