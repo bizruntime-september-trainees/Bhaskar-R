8e7813d884d9d918f7a1fb0dc79c92f38354a40a
Bhaskar5959 <bhaskar221998@gmail.com>
2020-09-07 19:20:20 +0530

    commit indexes files

---
+++ Indexes 7-09-2020/Indexers/Indexers/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Day = System.DayOfWeek;
+
+namespace SampleIndexers
+{
+    public class Employee
+    {
+        //Declare the properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public double Salary { get; set; }
+        public string Location { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        //Initialize the properties through constructor
+        public Employee(int ID, string Name, string Job, int Salary, string Location,
+                        string Department, string Gender)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Job = Job;
+            this.Salary = Salary;
+            this.Location = Location;
+            this.Department = Department;
+            this.Gender = Gender;
+        }
+        public object this[string Name]
+        {
+           
+            get
+            {
+                if (Name == "ID")
+                    return ID;
+                else if (Name == "Name")
+                    return Name;
+                else if (Name == "Job")
+                    return Job;
+                else if (Name == "Salary")
+                    return Salary;
+                else if (Name == "Location")
+                    return Location;
+                else if (Name == "Department")
+                    return Department;
+                else if (Name == "Gender")
+                    return Gender;
+                else
+                    return null;
+            }            
+            set
+            {
+                if (Name == "ID")
+                    ID = Convert.ToInt32(value);
+                else if (Name == "Name")
+                    Name = value.ToString();
+                else if (Name == "Job")
+                    Job = value.ToString();
+                else if (Name == "Salary")
+                    Salary = Convert.ToDouble(value);
+                else if (Name == "Location")
+                    Location = value.ToString();
+                else if (Name == "Department")
+                    Department = value.ToString();
+                else if (Name == "Gender")
+                    Gender = value.ToString();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(101, "Pranaya", "SSE", 10000, "Mumbai", "IT", "Male");
+            Console.WriteLine("EID = " + emp["ID"]);
+            Console.WriteLine("Name = " + emp["Name"]);
+            Console.WriteLine("Job = " + emp["Job"]);
+            Console.WriteLine("Salary = " + emp["Salary"]);
+            Console.WriteLine("Location = " + emp["Location"]);
+            Console.WriteLine("Department = " + emp["Department"]);
+            Console.WriteLine("Gender = " + emp["Gender"]);
+            emp["Name"] = "Kumar";
+            emp["salary"] = 65000;
+            emp["Location"] = "BBSR";
+            Console.WriteLine("After changing index values:");
+            Console.WriteLine("EID = " + emp["ID"]);
+            Console.WriteLine("Name = " + emp["Name"]);
+            Console.WriteLine("Job = " + emp["Job"]);
+            Console.WriteLine("Salary = " + emp["Salary"]);
+            Console.WriteLine("Location = " + emp["Location"]);
+            Console.WriteLine("Department = " + emp["Department"]);
+            Console.WriteLine("Gender = " + emp["Gender"]);            
+        }
+    }
+}
---
+++ Indexes 7-09-2020/IndexersWithListEmployee/IndexersWithListEmployee/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IndexersWithListEmployee
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+    }
+
+    public class Company
+    {        
+        private List<Employee> listEmployees;        
+        public Company()
+        {
+            listEmployees = new List<Employee>();
+            listEmployees.Add(new Employee
+            { EmployeeId = 101, Name = "Pranaya", Gender = "Male", Salary = 1000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 102, Name = "Preety", Gender = "Female", Salary = 2000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 103, Name = "Anurag", Gender = "Male", Salary = 5000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 104, Name = "Priyanka", Gender = "Female", Salary = 4000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 105, Name = "Hina", Gender = "Female", Salary = 3000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 106, Name = "Sambit", Gender = "Male", Salary = 6000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 107, Name = "Tarun", Gender = "Male", Salary = 8000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 108, Name = "Santosh", Gender = "Male", Salary = 7000 });
+            listEmployees.Add(new Employee
+            { EmployeeId = 109, Name = "Trupti", Gender = "Female", Salary = 5000 });
+        }
+       
+        public string this[int employeeId]
+        {
+            get
+            {
+                return listEmployees.
+                    FirstOrDefault(x => x.EmployeeId == employeeId).Name;
+            }
+            set
+            {
+                listEmployees.
+                    FirstOrDefault(x => x.EmployeeId == employeeId).Name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+            Console.WriteLine("Name of Employee with Id = 101: " + company[101]);
+            Console.WriteLine();
+            Console.WriteLine("Name of Employee with Id = 105: " + company[105]);
+            Console.WriteLine();
+            Console.WriteLine("Name of Employee with Id = 107: " + company[107]);
+            Console.WriteLine();
+
+        }
+    }
+}
