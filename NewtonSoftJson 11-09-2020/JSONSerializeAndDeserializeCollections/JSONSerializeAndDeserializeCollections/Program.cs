a8f26611012e475e23dc28657e316b4564b861d0
Bhaskar5959 <bhaskar221998@gmail.com>
2020-09-11 23:49:59 +0530

    Commit some more files

---
+++ NewtonSoftJson 11-09-2020/JSONSerializeAndDeserializeCollections/JSONSerializeAndDeserializeCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace JSONSerializationAndDeserialization
+{
+    class Product
+    {
+        public String Name { set; get; }
+        public Double Price { set; get; }
+        
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>() {
+                        new Product(){Name="apple",Price = 10.90},
+                        new Product(){Name = "Banana",Price=8.40},
+                        new Product(){Name="custardApple",Price=12.50},
+                        new Product(){Name="Orange",Price=10.20}
+
+            };
+           
+
+            string result = JsonConvert.SerializeObject(list);
+            Console.WriteLine(result);
+
+            List<Product>items = JsonConvert.DeserializeObject <List<Product>>(result);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Price);
+            }
+        }
+    }
+}
