5a47a9ee8b9339fa6af99b2df5ebdaf0000ee966
Bhaskar5959 <bhaskar221998@gmail.com>
2020-09-28 09:54:28 +0530

    commit ReactivePogramming with subject

---
+++ ReactiveProgramming 23-09-2020/ReactiveProgrammingUsingSubject 24-09-2020/ReactiveWithSubject/ReactiveWithSubject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveWithSubject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject<String> obj = new Subject<string>();
+            Observable.Interval(TimeSpan.FromSeconds(1))
+                .Select(x => "First" + x)
+                .Take(7)
+                .Subscribe(obj);
+            Observable.Interval(TimeSpan.FromSeconds(2))
+                .Select(x => "Second" + x)
+                .Take(5)
+                .Subscribe(obj);
+            var subscription = obj.Subscribe(Console.WriteLine);
+           
+           
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ReactiveProgramming 23-09-2020/ReactiveProgrammingUsingSubject 24-09-2020/UsingSubjectRX/UsingSubjectRX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingSubjectRX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject<int> sub = new Subject<int>();
+
+           var dispose= sub.Subscribe(Console.WriteLine);
+
+            sub.OnNext(5);
+            sub.OnNext(8);
+            dispose.Dispose();
+            sub.OnNext(56);
+            Console.ReadKey();
+        }
+    }
+}
